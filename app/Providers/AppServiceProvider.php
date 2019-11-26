f6848e59c625d6cfc1b364967762b6a9b8fd25b3
felixmontes <felixmontes@gmail.com>
2019-11-25 19:44:25 -0500

    prueba-felix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,21 +25,5 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
-<<<<<<< HEAD
-=======
-         
-    
-        Validator::extend('alpha_spaces', function ($attribute, $value) {
-
-            // This will only accept alpha and spaces.
-            // If you want to accept hyphens use: /^[\pL\s-]+$/u.
-            return preg_match('/^[\pL\s]+$/u', $value);
-           
-        });
-        
-        Validator::extend('uppercase', function ($attribute, $value) {
-            return preg_match('@[A-Z]@', $value);
-        });
->>>>>>> origin/felix
     }
 }
